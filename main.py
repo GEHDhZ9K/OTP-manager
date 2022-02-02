5b272f1f56104578b9ea5f41b61ad9476222ebde
niromru <emailr47@gmail.com>
2022-02-02 16:39:14 +0545

    removes the error if you run it in the main directory

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 
 import os, json, sys
 
-os.chdir(os.path.dirname(sys.argv[0]))
+path = os.path.abspath(__file__).split("/")
+del path[-1]
+if os.getcwd() != "/".join(path):
+  os.chdir(os.path.dirname(sys.argv[0]))
 
 name = input(str("Enter the name: "))
 secret = input(str("Enter the secret: "))
+
+def create_json():
+  with open("./data/storage.json", "w") as f:
+    data = {"keys" :{}}
+    json.dump(data, f, indent=2)
+
+
+if "data" not in os.listdir():
+  os.mkdir("data")
+  create_json()
+elif "storage.json" not in os.listdir("data"):
+  create_json()
 
 def write_dict(data, file="./data/storage.json"):
   with open(file, "w") as f:
